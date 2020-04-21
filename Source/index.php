f198c78e26e338c8db615670daa95b57853cae49
hoanganh-PC\hoanganh <anhpt@gemiso.com>
2020-04-21 19:52:01 +0700

    no message

--- Source/index.php
+++ Source/index.php
@@ -1,11 +1,15 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+
+
 require_once 'core/init.php';
 require_once 'includes/header.php';
 
 if ($user){
 	require_once 'templates/main.php';
 }else{
-    require_once 'templates/auth/login.php';
+    // require_once 'templates/auth/login.php';
+    require_once 'templates/main.php';
 }
 
 require_once 'includes/footer.php';
